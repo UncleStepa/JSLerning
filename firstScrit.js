c2a196531af75fd895dacbb4f6f7a99f4650f4c8
Igor <example@gmail.com>
2019-08-10 20:03:40 +0400

    last commit for second lesson

--- firstScrit.js
+++ firstScrit.js
@@ -11,19 +11,19 @@ let appData = {
     savings: false
 };
 
-    // for (let i = 0; i < 2; i++) {
-    //     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    //         b = prompt("Во сколько обойдется?", "");
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt("Во сколько обойдется?", "");
 
-    //     if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-    //     && a != '' && b != '' && a.length < 50) {
-    //         console.log("done");
-    //         appData.expenses[a] = b;
-    //     }
+        if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 50) {
+            console.log("done");
+            appData.expenses[a] = b;
+        }
         
-    // };
+    };
 
-    let i = 0;
+    // let i = 0;
     // while (i < 2) {
     //     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
     //     b = prompt("Во сколько обойдется?", "");
@@ -36,18 +36,19 @@ let appData = {
     // i++;
     // }
 
-    do {
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt("Во сколько обойдется?", "");
+    // do {
+    //     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    //     b = prompt("Во сколько обойдется?", "");
 
-    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-    && a != '' && b != '' && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
-    }
-    i++;
-    }
-    while (i < 2);
+    // if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+    // && a != '' && b != '' && a.length < 50) {
+    //     console.log("done");
+    //     appData.expenses[a] = b;
+    // }
+    // i++;
+    // }
+    // while (i < 2);
+    
     appData.moneyPerDay = appData.budget / 30;
 
     alert("Ежедневный бюджет: " + appData.moneyPerDay);
